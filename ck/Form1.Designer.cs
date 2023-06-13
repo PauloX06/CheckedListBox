1dc79e279e1098c80dfc92ad646658db30e79f13
Paulo Henrique <paulo.souza@estudante.ifro.edu.br>
2023-06-12 21:08:12 -0400

    CheckedListBox

---
+++ ck/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.ck2 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(457, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // ck2
+            // 
+            this.ck2.FormattingEnabled = true;
+            this.ck2.Location = new System.Drawing.Point(27, 54);
+            this.ck2.Name = "ck2";
+            this.ck2.Size = new System.Drawing.Size(167, 38);
+            this.ck2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(630, 303);
+            this.Controls.Add(this.ck2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox ck2;
+    }
+}
+
---
+++ ck/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarItens();
+        }
+
+        private void CarregarItens()
+        {
+            ck2.Items.Add("Pasta de dente");
+            ck2.Items.Add("Fita Led");
+            ck2.Items.Add("Hamburger");
+            ck2.Items.Add("Pizza");
+            ck2.Items.Add("Lasanha");
+        }
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string itens = "";
+            foreach (var item in ck2.CheckedItems)
+            {
+                 itens += item.ToString() + "\n";
+            }
+
+            MessageBox.Show("Itens selecionados:\n" + itens);
+        }
+    }
+}
